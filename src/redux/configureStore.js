e2d8585ad6e8dc816e0f09f050e5c71b19866792
waqas ahmad <waqaskanju@gmail.com>
2022-07-27 22:43:53 +0500

    configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,7 @@ import logger from 'redux-logger';
 import { loadingBarReducer } from 'react-redux-loading-bar';
 import continentReducer, { fetchContinent } from './countries/countries';
 
+// Combine c
 const store = configureStore({
   reducer: {
     loadingBar: loadingBarReducer,
--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,38 +1,32 @@
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
 import * as API from './api';
 
-// Create action type.
+// Create action type constant.
 const LOAD_CONTINENT = 'cases/load-continent';
 const LOAD_COUNTRY = 'cases/load-country';
 
+// add action type to load continent data.
 export const loadContinent = (payload) => ({
   type: LOAD_CONTINENT,
   payload,
 });
 
+// add action type to load country data.
 export const loadCountry = (payload) => ({
   type: LOAD_COUNTRY,
   payload,
 });
 
-export const fetchCountry = (name) => async (dispatch) => {
-  dispatch(showLoading());
-  const data = await API.getCountry(name);
-
-  dispatch(loadCountry(data));
-  dispatch(hideLoading());
-};
-
+// Fetch data about a continent
 export const fetchContinent = (continent) => async (dispatch) => {
+  // show loading bar when loading data.
   dispatch(showLoading());
   const map = await API.getContinent(continent);
 
   const data = Object.values(map).reduce((accumulator, currentValue) => {
     const { All: { country, confirmed } } = currentValue;
-
     accumulator.items.push({ name: country, confirmed });
     accumulator.totalConfirmed += confirmed;
-
     return accumulator;
   }, {
     totalConfirmed: 0,
@@ -45,12 +39,23 @@ export const fetchContinent = (continent) => async (dispatch) => {
   dispatch(hideLoading());
 };
 
+// Fetch data when a country name is given.
+export const fetchCountry = (name) => async (dispatch) => {
+  // Show the loading bar when geting data.
+  dispatch(showLoading());
+  const data = await API.getCountry(name);
+  dispatch(loadCountry(data));
+  // hide loading bar then loading is finished.
+  dispatch(hideLoading());
+};
+
 const initialState = {
   totalConfirmed: 0,
   items: [],
   selected: null,
 };
 
+// Take appropirate action based on action type send.
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_CONTINENT:
